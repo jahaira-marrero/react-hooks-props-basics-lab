b1cb13ca511bc5c29a183aa11f7ff6857dde8bff
Jahaira <jahaira.marrero@gmail.com>
2021-03-15 23:34:07 -0400

    Done.

--- src/data/user.js
+++ src/data/user.js
@@ -2,7 +2,7 @@ const user = {
   name: "Liza",
   city: "New York",
   bio: "I made this!",
-  color: "firebrick",
+  color: 'firebrick',
   links: {
     github: "https://github.com/liza",
     linkedin: "https://www.linkedin.com/in/liza/",
